107342fbcbebb4021cd5af003b49a0890183e9b7
DimonDimskiy <skdmyr@gmail.com>
2022-12-23 18:38:58 +0100

    init

--- scripts/Практика 3/b_Самостоятельная + домашняя работа/b_systeminfo_widget.py
+++ scripts/Практика 3/b_Самостоятельная + домашняя работа/b_systeminfo_widget.py
@@ -1,3 +1,82 @@
+from PySide6 import QtWidgets, QtCore
+
+from a_threads import SystemInfo
+
+class Window(QtWidgets.QWidget):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.thread = None
+        self.initTreads()
+        self.initUI()
+        self.initSignals()
+
+
+    def initTreads(self):
+
+        self.thread = SystemInfo()
+        self.thread.start()
+
+    def initUI(self):
+
+        self.delaySpinBox = QtWidgets.QSpinBox()
+        self.delaySpinBox.setRange(1, 60)
+        self.delayLabel = QtWidgets.QLabel("Задержка обновления:")
+
+        self.cpuLoad = QtWidgets.QLCDNumber()
+        self.cpuLabel = QtWidgets.QLabel("Загрузка процессора:")
+        self.ramLoad = QtWidgets.QLCDNumber()
+        self.ramLabel = QtWidgets.QLabel("Использование памяти:")
+
+        ramLayout = QtWidgets.QHBoxLayout()
+        ramLayout.addWidget(self.ramLabel)
+        ramLayout.addWidget(self.ramLoad)
+
+        cpuLayout = QtWidgets.QHBoxLayout()
+        cpuLayout.addWidget(self.cpuLabel)
+        cpuLayout.addWidget(self.cpuLoad)
+
+        delayLayout = QtWidgets.QHBoxLayout()
+        delayLayout.addWidget(self.delayLabel)
+        delayLayout.addWidget(self.delaySpinBox)
+
+        mainLayout = QtWidgets.QVBoxLayout()
+        mainLayout.addLayout(delayLayout)
+        mainLayout.addLayout(cpuLayout)
+        mainLayout.addLayout(ramLayout)
+
+        self.setLayout(mainLayout)
+
+        self.setMinimumSize(300, 120)
+
+    def initSignals(self):
+        self.delaySpinBox.valueChanged.connect(lambda x: self.thread.setDelay(x))
+        self.thread.systemInfoReceived.connect(self.onSystemInfoReceived)
+
+    def onSystemInfoReceived(self, info):
+
+        self.cpuLoad.display(f"{info[0]}%")
+        self.ramLoad.display(f"{info[1]}%")
+
+    def closeEvent(self, event: QtCore.QEvent) -> None:
+
+        self.thread.quit()
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication()
+
+    window = Window()
+    window.show()
+
+    app.exec()
+
+
+
+
+
+
 """
 Реализовать виджет, который будет работать с потоком SystemInfo из модуля a_threads
 
